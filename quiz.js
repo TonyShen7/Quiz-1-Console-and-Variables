713b3cd40988875e537eb8067ef87ded0f16d575
TonyShen7 <Tony.shen@stu.ocsb.ca>
2024-03-21 13:33:21 -0400

    e

--- quiz.js
+++ quiz.js
@@ -1,9 +1,29 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
-
+let temperature = prompt("Please type the Fahrenheit you want to change to Celsius.");
+let calculation = 32
+let mul = 0.5556
+console.log(temperature-calculation*mul);
 //Program 2: Grade Calculator
-
+let grade = prompt("What is your grade?");
+let c = 70 - 80;
+let a = 90 = 100;
+let b = 80 - 90;
+let d = 60 - 70
+if (grade>90){
+    console.log("A")
+}
+else if (grade>80<90){
+ console.log("B")
+}
+else if (grade>70<80){
+    console.log("C")
+}
+else if (grade>60<70){
+    console.log("D")
+}
+console.log( grade );
 //Program 3: Leap Year Checker
 
 //Program 4: Largest Number Finder
